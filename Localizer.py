411728429083e2f36a691b8db7966f91a1ea6a1f
Chris Darch <comradenapcal@aol.com>
2020-06-08 00:34:38 -0700

    piratespinball: Remove header.

--- Localizer.py
+++ Localizer.py
@@ -1,8 +1,3 @@
-# uncompyle6 version 3.7.0
-# Python bytecode 2.4 (62061)
-# Decompiled from: PyPy Python 3.6.9 (2ad108f17bdb, Apr 07 2020, 03:05:35)
-# [PyPy 7.3.1 with MSC v.1912 32 bit]
-# Embedded file name: Localizer.py
 """
 This module determines what language to run the game in
 and imports the appropriate language module.
--- RunPiratePinball.py
+++ RunPiratePinball.py
@@ -1,8 +1,3 @@
-# uncompyle6 version 3.7.0
-# Python bytecode 2.4 (62061)
-# Decompiled from: PyPy Python 3.6.9 (2ad108f17bdb, Apr 07 2020, 03:05:35)
-# [PyPy 7.3.1 with MSC v.1912 32 bit]
-# Embedded file name: RunPiratePinball.py
 import string, re, codecs
 from random import randint
 from winreg import *
